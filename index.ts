e2ae01c32e0dc51d389445545df458a8cf961548
Omar <oochurquina@hotmail.com>
2021-08-16 22:23:11 -0300

    tercer commit

--- index.ts
+++ index.ts
@@ -12,5 +12,5 @@ server.app.use(cors({origin: true, credentials: true}));
 server.app.use('/', router);
 
 server.start(()=>{
-    console.log(`Servidor corriendo en el puerto ${server.port}`)
+    console.log(`Servidor corriendo en el puerto: ${server.port}`)
 })
